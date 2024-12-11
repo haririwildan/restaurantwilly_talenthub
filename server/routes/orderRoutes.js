d14bd53ec292054ed75588318838234f5b3488dc
haririwildan <wildanhariri24@gmail.com>
2024-12-11 22:05:38 +0700

    add get order details

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router()
 const orderController = require('../controllers/orderController')
 
+// to add order
 router.post('/', orderController.addOrder);
+
+// to get order details
+router.get('/:id', orderController.getOrderDetails);
 
 module.exports = router;
