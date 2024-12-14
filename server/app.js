a7382bc94c54f72ba6db36620bc5d383b994cae4
haririwildan <wildanhariri24@gmail.com>
2024-12-14 10:41:39 +0700

    vercel connect

--- server/app.js
+++ server/app.js
@@ -51,3 +51,6 @@ app.use('/order', orderRoutes);
 // local
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+
+// vercel connect
+module.exports = app;
